73ec324c2eccbcb64ab32c6807932a9dc3919e73
Billie <130059458+Shogiita@users.noreply.github.com>
2026-01-27 09:05:27 +0700

    Revert "new media endpoint update"
    This reverts commit 11dd2d363b533eec221aa922ba982ddbef3dd95f.

--- app/routers/instagram_router.py
+++ app/routers/instagram_router.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Body
-from pydantic import BaseModel, Field
+from fastapi import APIRouter, Query
 from app.controllers import instagram_controller
 
 router = APIRouter(
@@ -7,24 +6,15 @@ router = APIRouter(
     tags=["Instagram"]
 )
 
-class MediaRequest(BaseModel):
-    limit: int = Field(
-        default=25, 
-        ge=1, 
-        le=10000, 
-        description="Jumlah postingan yang ingin diambil (Maksimal 10000)"
-    )
-
 @router.get("/profile")
 async def get_profile_endpoint():
     return await instagram_controller.get_user_profile()
 
-
-@router.post("/media")
-async def get_media_endpoint(request: MediaRequest):
-    limit_value = request.limit
-    
-    return await instagram_controller.get_user_media(limit=limit_value)
+@router.get("/media")
+async def get_media_endpoint(
+    limit: int = Query(100, ge=1, le=100, description="Jumlah postingan yang ingin diambil (antara 1 dan 100)")
+):
+    return await instagram_controller.get_user_media(limit=limit)
 
 @router.get("/debug-token")
 async def debug_token_endpoint():
