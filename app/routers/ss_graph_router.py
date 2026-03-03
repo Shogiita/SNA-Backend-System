af1c7da3ff3c43ec323837e4659be7959a6eff21
Billie <130059458+Shogiita@users.noreply.github.com>
2026-03-03 13:53:22 +0700

    cloudbuild update

--- app/routers/ss_graph_router.py
+++ app/routers/ss_graph_router.py
@@ -9,10 +9,7 @@ router = APIRouter(
 
 class GraphLimitRequest(BaseModel):
     limit: Optional[int] = 1000
+
 @router.post("/snagraph")
 async def create_ss_graph_endpoint(req: GraphLimitRequest):
-    """
-    Endpoint untuk membuat graf SNA terhubung beserta metriknya (Degree, Betweenness, dll)
-    bersumber LANGSUNG dari Neo4j Graph Database.
-    """
     return await neo4j_graph_controller.create_graph_from_neo4j(limit=req.limit)
